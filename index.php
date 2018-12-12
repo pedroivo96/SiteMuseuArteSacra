7bfa46ce9f8f9a27eb75b65d7f6918d06038290b
pedroivo96 <soaresbarbosapedroivo@gmail.com>
2018-12-12 17:11:33 -0300

    fundo preto

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
     <meta name="description" content="Source code generated using layoutit.com">
     <meta name="author" content="LayoutIt!">
 	
-	<link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+	<link href="css3/bootstrap.css" rel="stylesheet">
+    <link href="css3/bootstrap.min.css" rel="stylesheet">
+    <link href="css3/style.css" rel="stylesheet">
 	
   </head>
   <body class="bg-dark">
